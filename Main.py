3e0955f7590acfa84a138f99110114e210149a97
Dawid Sojka <dawid.sojka@globallogic.com>
2018-01-23 17:16:51 +0100

    First version of match filtering

--- Main.py
+++ Main.py
@@ -1,91 +1,65 @@
 from datetime import datetime
 from Menu import Menu
-from Match import Match
-import csv
 
 def main():
+    menu = Menu()
     matchesBase = []
-    baseStart = datetime.today()
-    baseEnd = datetime.today()
-    baseStart, baseEnd = readSavedData(matchesBase, baseStart, baseEnd)
+    baseStart, baseEnd = menu.readSavedData(matchesBase)
 
     matches = []
-    menu = Menu()
     print("Ustaw zakres dat do analizy")
     menu.getDates()
     baseStart, baseEnd = menu.downloadMatches(matches, matchesBase, baseStart, baseEnd)
     print("Mecze w zakresie: {}".format(len(matches)))
 
     filteredMatches = matches
+    filterKeys = []
+    filterValues = []
 
     while True:
-        print("Mozesz wyswietlic mecze, zmienic zakres dat lub dodac/usunac filtry")
-        print("Wpisz 'w' zeby wyswietlic, 'z' zeby zmenic zakres dat, 'f' zeby dodac filtr, 'u' zeby usunac filtr, lub 'q' zeby zakonczyc")
+        print("\nMozesz wyswietlic mecze, zmienic zakres dat lub dodac/usunac filtry")
+        print("Opcje:\n'w' - wyswiet wyniki\n'z' - zmien zakres dat\n'f' - dodaj filtr\n'u' - usun filtr\n'q' - zakoncz")
         while True:
-            chosenOption = input("Wybrana opcja ['w','z','f','u','q']: ")
+            chosenOption = input("Wybrana opcja: ")
             if (chosenOption.lower() == "w" or chosenOption.lower() == "f" or chosenOption.lower() == "u" or chosenOption.lower() == "z" or chosenOption.lower() == "q"):
                 break
             else:
                 print("Bledny wybor. Sprobuj ponownie")
+
         if (chosenOption.lower() == "w"):
             for match in filteredMatches:
                 match.printMatchRow()
+
         elif (chosenOption.lower() == 'z'):
             print("Ustaw zakres dat do analizy")
             menu.getDates()
             baseStart, baseEnd = menu.downloadMatches(matches,matchesBase, baseStart, baseEnd)
             print("Mecze w zakresie: {}".format(len(matches)))
+
         elif (chosenOption.lower() == 'q'):
             break
-        #elif chosenOption.lower() == 'f':
+
+        elif chosenOption.lower() == 'f':
+            print("Wybierz kategorie filtra")
+            print("'l' - liga\n'd' - druzyna")
+            while True:
+                filterType = input("Wybrana opcja: ")
+                if filterType.lower() == "l" or filterType.lower() == "d":
+                    break
+                else:
+                    print("Bledny wybor. Sprobuj ponownie")
+
+            filterExpr = input("Podaj filtrowane wyrazenie: ")
+
+            filterKeys.append(filterType)
+            filterValues.append(filterExpr)
+
+            filteredMatches = menu.filterMatches(matches, filterKeys, filterValues)
 
 
 
     # longestTeam = menu.longestTeam(matches)
     # print(longestTeam)
-
-def readSavedData(matches, baseStart, baseEnd):
-    try:
-        file = open("baza.csv", "r", newline='')
-        reader = csv.reader(file)
-        rownum = 0
-
-        for row in reader:
-            if rownum == 0:
-                baseStart = datetime.strptime(row[0], "%d %m %Y")
-                baseEnd = datetime.strptime(row[1], "%d %m %Y")
-            else:
-                match = Match()
-                match.league = row[0]
-                match.timeStart = datetime.strptime(row[1], "%d %m %Y %H:%M")
-                match.homeTeam = row[2]
-                match.awayTeam = row[3]
-                match.predProbHome = row[4]
-                match.predProbDraw = row[5]
-                match.predProbAway = row[6]
-                match.predTips = row[7]
-                match.oddsHome = row[8]
-                match.oddsDraw = row[9]
-                match.oddsAway = row[10]
-                match.predScore1 = (row[11],row[12])
-                match.predScore2 = (row[13],row[14])
-                match.predUnder = row[15]
-                match.predOver = row[16]
-                match.matchScore = (row[17],row[18])
-                matches.append(match)
-            rownum += 1
-
-        file.close()
-        print("Wczytano {} meczow z bazy".format(rownum-1))
-        return baseStart, baseEnd
-
-    except FileNotFoundError:
-        print("Nie znaleziono pliku z baza meczow, bedzie stworzona nowa baza")
-        file = open("baza.csv", "w", newline='')
-        writer = csv.writer(file)
-        writer.writerow([baseStart.strftime("%d %m %Y"), baseEnd.strftime("%d %m %Y")])
-        file.close()
-        return baseStart, baseEnd
 
 if __name__ == "__main__":
     main()
--- Menu.py
+++ Menu.py
@@ -1,11 +1,58 @@
 from datetime import timedelta, datetime
 from ScrapPage import scrapPage
+from Match import Match
+import csv
 
 class Menu:
 
     startDate = datetime.today()
     endDate = datetime.today()
     today = datetime.today()
+
+    def readSavedData(self, matches):
+        try:
+            file = open("baza.csv", "r", newline='')
+            reader = csv.reader(file)
+            rownum = 0
+
+            for row in reader:
+                if rownum == 0:
+                    baseStart = datetime.strptime(row[0], "%d %m %Y")
+                    baseEnd = datetime.strptime(row[1], "%d %m %Y")
+                else:
+                    match = Match()
+                    match.league = row[0]
+                    match.timeStart = datetime.strptime(row[1], "%d %m %Y %H:%M")
+                    match.homeTeam = row[2]
+                    match.awayTeam = row[3]
+                    match.predProbHome = row[4]
+                    match.predProbDraw = row[5]
+                    match.predProbAway = row[6]
+                    match.predTips = row[7]
+                    match.oddsHome = row[8]
+                    match.oddsDraw = row[9]
+                    match.oddsAway = row[10]
+                    match.predScore1 = (row[11], row[12])
+                    match.predScore2 = (row[13], row[14])
+                    match.predUnder = row[15]
+                    match.predOver = row[16]
+                    match.matchScore = (row[17], row[18])
+                    matches.append(match)
+                rownum += 1
+
+            file.close()
+            print("Wczytano {} meczow z bazy".format(rownum - 1))
+            return baseStart, baseEnd
+
+        except FileNotFoundError:
+            print("Nie znaleziono pliku z baza meczow, bedzie stworzona nowa baza")
+            file = open("baza.csv", "w", newline='')
+            writer = csv.writer(file)
+            writer.writerow([baseStart.strftime("%d %m %Y"), baseEnd.strftime("%d %m %Y")])
+            file.close()
+            baseStart = datetime.today()
+            baseEnd = datetime.today()
+            return baseStart, baseEnd
 
     def getDates(self):
         while True:
@@ -40,13 +87,18 @@ class Menu:
         matches.clear()
         baseDateList = self.setDatesRange(baseStart, baseEnd)
         tempDateList = self.setDatesRange(self.startDate, self.endDate)
+        splitDateList = []
 
         if baseEnd.date() == self.today.date():
             if baseStart.date() == self.today.date():
                 baseEnd = self.endDate
         elif self.endDate.date() > baseEnd.date():
+            if self.startDate.date() > baseEnd.date():
+                splitDateList = self.setDatesRange(baseEnd + timedelta(1), self.startDate - timedelta(1))
             baseEnd = self.endDate
         if self.startDate.date() < baseStart.date():
+            if self.endDate.date() < baseStart.date():
+                splitDateList = self.setDatesRange(self.endDate + timedelta(1), baseStart - timedelta(1))
             baseStart = self.startDate
 
         file = open("baza.csv", "a", newline='')
@@ -54,6 +106,9 @@ class Menu:
         for date in tempDateList:
             if date not in baseDateList:
                 scrapPage(date, matchesBase, file)
+
+        for date in splitDateList:
+            scrapPage(date,matchesBase, file)
 
         file.close()
         file = open("baza.csv", "r+", newline='')
@@ -69,20 +124,21 @@ class Menu:
 
         return baseStart, baseEnd
 
-        # removeList = []
-        # for date in self.datesList:
-        #     if date not in tempDateList:
-        #         for x in matches:
-        #             if x.timeStart.date() == date.date():
-        #                 removeList.append(x)
-        # if len(removeList) > 0:
-        #     print("Usunieto {} meczow".format(len(removeList)))
-        # for x in removeList:
-        #     matches.remove(x)
-        # for date in tempDateList:
-        #     if date not in self.datesList:
-        #         scrapPage(date, matches)
-        # self.datesList = tempDateList
+    def filterMatches(self, matches, filterKeys, filterValues):
+        filteredMatches = []
+        nFilters = len(filterKeys)
+        for n in range(nFilters):
+            if filterKeys[n] == "l":
+                for match in matches:
+                    if match.league == filterValues[n]:
+                        if match not in filteredMatches:
+                            filteredMatches.append(match)
+            elif filterKeys[n] == "d":
+                for match in matches:
+                    if match.homeTeam == filterValues[n] or match.awayTeam == filterValues[n]:
+                        if match not in filteredMatches:
+                            filteredMatches.append(match)
+        return filteredMatches
 
     def displayCorrectMatches(self, matches):
         counter = 0
